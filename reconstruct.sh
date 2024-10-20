c9c5b71379e0aa3f647229a687a5dd19c99a27d4
pfc <96251537+pfcdx@users.noreply.github.com>
2024-10-20 18:47:11 +0300

    Delete reconstruct.sh

--- reconstruct.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-read -e -p "Enter folder path: " path
-
-cd $path
-
-for folder in *-extracted/; do
-    zip -r "${folder%-extracted/}-upscaled.zip" "$folder" && rm -rf "$folder"
-done
