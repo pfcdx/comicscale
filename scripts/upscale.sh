80c82d915d49a18dd1504acbb048e7b18096ef5d
pfc <96251537+pfcdx@users.noreply.github.com>
2024-10-20 19:37:49 +0300

    Update upscale.sh

--- scripts/upscale.sh
+++ scripts/upscale.sh
@@ -2,7 +2,7 @@
 
 ### Upscales only one folder's contents ###
 
-REALESRGAN_EXECUTABLE_PATH=""
+REALESRGAN_EXECUTABLE_PATH="" # Enter path of realesrgan-ncnn-vulkan executable
 
 read -e -p "Enter folder path: " path
 
@@ -11,7 +11,7 @@ cd "$path" || exit
 process=0
 for file in *.jpg *.jpeg *.png; do
     if [[ "$file" != *-scaled.* ]]; then
-        "$REALESRGAN_PATH" -i "$file" -o "${file%.*}-scaled.png" -n realesrgan-x4plus-anime -s 2 &
+        "$REALESRGAN_EXECUTABLE_PATH" -i "$file" -o "${file%.*}-scaled.png" -n realesrgan-x4plus-anime -s 2 &
         process=$((process+1))
         if [[ $process -ge 2 ]]; then # Means 2 processes atime, you can increase that if you want
             wait -n
