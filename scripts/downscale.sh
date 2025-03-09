6e129be7b662bfb151ec96127a1ecd808e4fb5c1
pfc <96251537+pfcdx@users.noreply.github.com>
2025-03-09 23:02:54 +0300

    Update downscale.sh

--- scripts/downscale.sh
+++ scripts/downscale.sh
@@ -9,7 +9,7 @@ cd "$path" || exit
 for file in *-scaled.*; do
     if [[ "$file" == *-scaled.* ]]; then
         # Default "upscale" script scales up 4 times.
-        # ..and this downscaler scales down 2 times.
+        # ...and this downscaler scales down 2 times.
         # You can change that if you want, too.
         convert -resize 50% "$file" "${file%-scaled}-downscaled.png" 
         rm -- *-scaled.*
