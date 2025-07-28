f22b2dfb400d49d04e578ec36940907331972021
pfc <96251537+pfcdx@users.noreply.github.com>
2025-07-28 15:15:18 +0300

    Update upscale.sh

--- scripts/upscale.sh
+++ scripts/upscale.sh
@@ -11,7 +11,7 @@ cd "$path" || exit
 process=0
 for file in *.jpg *.jpeg *.png; do
     if [[ "$file" != *-scaled.* ]]; then
-        "$REALESRGAN_EXECUTABLE_PATH" -i "$file" -o "${file%.*}-scaled.png" -n realesrgan-x4plus-anime -s 2 &
+        "$REALESRGAN_EXECUTABLE_PATH" -i "$file" -o "${file%.*}-scaled.png" -n realesrgan-x4plus-anime &
         process=$((process+1))
         if [[ $process -ge 2 ]]; then # Means 2 processes atime, you can increase that if you want
             wait -n
